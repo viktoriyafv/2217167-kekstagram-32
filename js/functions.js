d9c621e56a905a761307b264a0a192fd94ea5e86
Виктория <viktoriya_atm@mail.ru>
2024-07-04 10:06:16 +0300

    5.16. Функции возвращаются

--- js/functions.js
+++ js/functions.js
@@ -21,11 +21,14 @@ const checkTimeLength = (startTimeWork, endTimeWork, startTimeMeeting, timeMeeti
 
 // eslint-disable-next-line no-console
 console.log(checkTimeLength('08:00', '17:30', '14:00', 90));
-
-// console.log(checkTimeLength('8:0', '10:0', '8:0', 120));
-// console.log(checkTimeLength('08:00', '14:30', '14:00', 90));
-// console.log(checkTimeLength('14:00', '17:30', '08:0', 90));
-// console.log(checkTimeLength('8:00', '17:30', '08:00', 900));
+// eslint-disable-next-line no-console
+console.log(checkTimeLength('8:0', '10:0', '8:0', 120));
+// eslint-disable-next-line no-console
+console.log(checkTimeLength('08:00', '14:30', '14:00', 90));
+// eslint-disable-next-line no-console
+console.log(checkTimeLength('14:00', '17:30', '08:0', 90));
+// eslint-disable-next-line no-console
+console.log(checkTimeLength('8:00', '17:30', '08:00', 900));
 
 /* Функция для проверки длины строки */
 

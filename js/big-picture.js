af9a1b4293640dd46a103274b32213e3a16db4d2
viktoriyafv <152386470+viktoriyafv@users.noreply.github.com>
2024-07-16 15:01:34 +0300

    Module8-task2 (#12)
    * Открывается и закрывается (часть 2)
    
    * Открывается и закрывается (часть 2)
    
    * Добавление return в showMoreComments
    
    * убирает вывод в консоль
    
    * Исправляет добавление комментариев при открытии
    
    * Добавляет кнопку Загрузить еще / работает с ошибкой
    
    * Открывается и закрывается (часть 2)
    
    * Рефакторинг render-comments

--- js/big-picture.js
+++ js/big-picture.js
@@ -1,11 +1,11 @@
 import { isEscapeKey } from './util.js';
 import { indefications } from './const.js';
+import { renderCommentsList } from './render-comments.js';
 
 const body = document.querySelector('body');
 const bigPicture = document.querySelector('.big-picture');
-const commentsList = bigPicture.querySelector('.social__comments');
 const commentsLoader = bigPicture.querySelector('.comments-loader');
-const socialComments = bigPicture.querySelector('.social__comment-count');
+//const socialComments = bigPicture.querySelector('.social__comment-count');
 const closeButtonPicture = bigPicture.querySelector('.big-picture__cancel');
 
 const onEscKeydown = (evt) => {
@@ -15,61 +15,40 @@ const onEscKeydown = (evt) => {
   }
 };
 
-/* Список комментариев под фотографией: комментарии должны вставляться в блок .social__comments.
-Разметка каждого комментария должна выглядеть так:
-
-<li class="social__comment">
-  <img
-    class="social__picture"
-    src="{{аватар}}"
-    alt="{{имя комментатора}}"
-    width="35" height="35">
-  <p class="social__text">{{текст комментария}}</p>
-</li>*/
-
-const renderCommentsList = (comments) => {
-  document.querySelector('.social__comments').textContent = '';
-  comments.forEach((comment) => {
-    const newCommentContainer = document.createElement('li');
-    const userAvatar = document.createElement('img');
-    newCommentContainer.className = 'social__comment';
-    userAvatar.className = 'social__picture';
-    userAvatar.src = comment.avatar;
-    userAvatar.alt = comment.name;
-    userAvatar.width = indefications.USERAVATARWIDTH;
-    userAvatar.height = indefications.USERAVATARHEIGTH;
-    const textComment = document.createElement('p');
-    textComment.className = 'social__text';
-    textComment.textContent = comment.message;
-    newCommentContainer.appendChild(userAvatar);
-    newCommentContainer.appendChild(textComment);
-    commentsList.appendChild(newCommentContainer);
-  });
-};
-
-
 const renderPictureDetails = ({ url, likes, description, comments }) => {
   bigPicture.querySelector('.big-picture__img img').src = url;
   bigPicture.querySelector('.big-picture__img img').alt = description;
   bigPicture.querySelector('.likes-count').textContent = likes;
   bigPicture.querySelector('.social__caption').textContent = description;
   bigPicture.querySelector('.social__comment-total-count').textContent = comments.length;
-
-  if (indefications.COMMENTSAMOUNT < comments.length) {
-    bigPicture.querySelector('.social__comment-shown-count').textContent = indefications.COMMENTSAMOUNT;
-  } else {
+  if (indefications.COMMENTSAMOUNT >= comments.length) {
     bigPicture.querySelector('.social__comment-shown-count').textContent = comments.length;
+    commentsLoader.classList.add('hidden');
+  } else {
+    commentsLoader.classList.remove('hidden');
+    bigPicture.querySelector('.social__comment-shown-count').textContent = indefications.COMMENTSAMOUNT;
   }
 };
 
 const openBigPicture = (data) => {
+  let commentShown = 5;
   bigPicture.classList.remove('hidden');
   body.classList.add('modal-open');
-  commentsLoader.classList.add('hidden');
-  socialComments.classList.add('hidden');
   document.addEventListener('keydown', onEscKeydown);
   renderPictureDetails(data);
-  renderCommentsList(data.comments);
+  renderCommentsList(data.comments.slice(0, indefications.COMMENTSAMOUNT));
+  commentsLoader.addEventListener('click', () => {
+    commentShown += indefications.COMMENTSAMOUNT;
+    const currentComments = data.comments.slice(0, commentShown);
+    renderCommentsList(currentComments);
+    if (commentShown >= data.comments.length) {
+      bigPicture.querySelector('.social__comment-shown-count').textContent = data.comments.length;
+      commentsLoader.classList.add('hidden');
+    } else {
+      commentsLoader.classList.remove('hidden');
+      bigPicture.querySelector('.social__comment-shown-count').textContent = commentShown;
+    }
+  });
 };
 
 function closeBigPicture() {
@@ -82,4 +61,4 @@ closeButtonPicture.addEventListener('click', () => {
   closeBigPicture();
 });
 
-export { openBigPicture };
+export { openBigPicture, closeButtonPicture };

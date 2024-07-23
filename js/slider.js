871c179e173f0b15c4b917f6d9e8e4e436478770
Виктория <viktoriya_atm@mail.ru>
2024-07-23 11:30:51 +0300

    switch

--- js/slider.js
+++ js/slider.js
@@ -50,18 +50,26 @@ specialElementsArray.forEach((element) => {
   element.addEventListener('change', () => {
     imgUploadPreview.className = '';
     imgUploadPreview.classList.add(`effects__preview--${element.value}`);
-    if (element.value === 'none') {
-      sliderElementBlock.classList.add('hidden');
-    } else if (element.value === 'chrome') {
-      setFilter(FILTERSCONFIG.chrome);
-    } else if (element.value === 'sepia') {
-      setFilter(FILTERSCONFIG.sepia);
-    } else if (element.value === 'marvin') {
-      setFilter(FILTERSCONFIG.marvin);
-    } else if (element.value === 'phobos') {
-      setFilter(FILTERSCONFIG.phobos);
-    } else if (element.value === 'heat') {
-      setFilter(FILTERSCONFIG.phobos);
+    switch (element.value) {
+      case 'none':
+        sliderElementBlock.classList.add('hidden');
+        imgUploadPreview.style.filter = 'none';
+        break;
+      case 'chrome':
+        setFilter(FILTERSCONFIG.chrome);
+        break;
+      case 'sepia':
+        setFilter(FILTERSCONFIG.sepia);
+        break;
+      case 'marvin':
+        setFilter(FILTERSCONFIG.marvin);
+        break;
+      case 'phobos':
+        setFilter(FILTERSCONFIG.phobos);
+        break;
+      case 'heat':
+        setFilter(FILTERSCONFIG.heat);
+        break;
     }
   });
 });

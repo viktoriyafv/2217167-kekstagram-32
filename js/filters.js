0b3dbafe51f6446ff4d39d6613cd15d10f74182f
Виктория <viktoriya_atm@mail.ru>
2024-07-29 11:17:08 +0300

    Перламутровые пуговицы

--- js/filters.js
+++ js/filters.js
@@ -1,1 +1,58 @@
-//фильтры
+import { debounce, shuffle } from './util';
+import { generatePictures } from './rendering-picture';
+import { indefications } from './const.js';
+
+const filterForm = document.querySelector('.img-filters');
+const defaultFilter = document.querySelector('#filter-default');
+const randomFilter = document.querySelector('#filter-random');
+const discussedFilter = document.querySelector('#filter-discussed');
+
+let currentFilter = defaultFilter;
+const RERENDER_DELAY = 500;
+
+const pictureElements = () => document.querySelectorAll('.picture');
+const removePictures = () => {
+  pictureElements().forEach((picture) => picture.remove());
+};
+
+const filterByRandom = (picture) => {
+  const randomList = shuffle(picture.length);
+  const randomPictures = [];
+  randomList.slice(0, 10).forEach((randomNumber) => {
+    randomPictures.push(picture[randomNumber]);
+  });
+  // eslint-disable-next-line no-unreachable, no-console
+  console.log(randomPictures);
+  return randomPictures;
+};
+
+const filterByComments = (picture) => {
+  const pictureByComments = picture.slice(generatePictures(picture.slice(0, indefications.SIMILARPICTURECOUNT).sort((a, b) => a.comments.length < b.comments.length ? 1 : -1)));
+  return pictureByComments;
+};
+
+const addFilters = (picture) => {
+  filterForm.addEventListener('click', debounce((evt) => {
+    currentFilter.classList.remove('img-filters__button--active');
+    currentFilter = evt.target;
+    currentFilter.classList.add('img-filters__button--active');
+    switch (currentFilter) {
+      case defaultFilter:
+        removePictures();
+        generatePictures(picture.slice(0, indefications.SIMILARPICTURECOUNT));
+        break;
+      case randomFilter:
+        removePictures();
+        filterByRandom(picture.slice(0, 10));
+        break;
+      case discussedFilter:
+        removePictures();
+        filterByComments(picture);
+        break;
+    }
+  }),
+  RERENDER_DELAY
+  );
+};
+
+export { addFilters };
--- js/main.js
+++ js/main.js
@@ -3,15 +3,20 @@ import { setUserFormSubmit } from './valid.js';
 import { closeUploadOverlay } from './form.js';
 import { getData } from './api.js';
 import { onErrorDataForm } from './success-error.js';
+import { addFilters } from './filters.js';
 
+const imgFilters = document.querySelector('.img-filters');
 const SIMILAR_PICTURE_COUNT = 25;
 
 getData()
-  .then((pictures) => {
-    generatePictures(pictures.slice(0, SIMILAR_PICTURE_COUNT));
+  .then((picture) => {
+    generatePictures(picture.slice(0, SIMILAR_PICTURE_COUNT));
+    imgFilters.classList.remove('img-filters--inactive');
+    addFilters(picture);
   })
   .catch(() => {
     onErrorDataForm();
+    imgFilters.classList.add('img-filters--inactive');
   });
 
 setUserFormSubmit(closeUploadOverlay);
